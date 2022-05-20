c1998d8c99af1bfd84e826662c01d54af30b5823
Kevin Loeffler <hi@kevinloeffler.ch>
2022-05-20 15:25:17 +0200

    ADD: import data from csv

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 import keras
 
-print('Hello World')
+train_test_split = 0.8
+plt.rcParams["figure.figsize"] = (10, 6)  # make matplotlib figures bigger
 
+# Import Data
+dataset_raw = pd.read_csv('data/training_data.csv')
