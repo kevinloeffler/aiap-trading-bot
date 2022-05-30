7b0bdc43923a3ac24fc2a2e96718838e3034e65e
Kevin Loeffler <hi@kevinloeffler.ch>
2022-05-30 14:08:04 +0200

    UPDATE: Use sklearn MinMaxScaler to normalize data

--- predict.py
+++ predict.py
@@ -1,6 +1,6 @@
 import keras
 import numpy as np
-from main import scaler
+from sklearn.preprocessing import MinMaxScaler
 
 model = keras.models.load_model('model')
 
@@ -10,14 +10,14 @@ start_price = 160.
 for i in range(60):
     test_data.append(start_price + i / 2)
 
-test_data = np.array(test_data)
-test_data = test_data.reshape(-1, 1)
+test_data = np.array(test_data).reshape(-1, 1)
 
+scaler = MinMaxScaler(feature_range=(0, 1))
 scaled_data = scaler.fit_transform(test_data)
 
 x_test = np.reshape(scaled_data, (1, 60, 1))
 
-prediction = model.predict(x_test)
-prediction = scaler.inverse_transform(prediction)
+prediction_raw = model.predict(x_test)
+prediction = scaler.inverse_transform(prediction_raw)
 
 print(prediction)
