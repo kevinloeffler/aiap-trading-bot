f60b9407bfd564d0451b18b6b69a8b186ec58103
Kevin Loeffler <hi@kevinloeffler.ch>
2022-05-31 19:50:33 +0200

    ADD: Prediction function based on price list

--- predict.py
+++ predict.py
@@ -2,6 +2,24 @@ import keras
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 
+
+def predict(prices: list) -> float:
+    model: keras.models.Model = keras.models.load_model('model')
+    scaler: MinMaxScaler = MinMaxScaler(feature_range=(0, 1))
+    last_price: int = prices[-1]
+    # Prepare data
+    prices_array = np.array(prices).reshape(-1, 1)
+    scaled_prices = scaler.fit_transform(prices_array)
+    prices_array = np.reshape(scaled_prices, (1, 60, 1))
+    # Predict
+    prediction_raw = model.predict(prices_array)
+    prediction = scaler.inverse_transform(prediction_raw)
+    # Translate to action
+    # ToDo: translate prediction to action
+    return prediction
+
+
+'''
 model = keras.models.load_model('model')
 
 test_data = []
@@ -21,3 +39,4 @@ prediction_raw = model.predict(x_test)
 prediction = scaler.inverse_transform(prediction_raw)
 
 print(prediction)
+'''
