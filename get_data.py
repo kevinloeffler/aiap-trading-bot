a21e382a2ce207b4900fe1425ae726f43fdbf51b
Kevin Loeffler <hi@kevinloeffler.ch>
2022-06-01 17:44:44 +0200

    ADD: Differentiation for crypto / stocks

--- get_data.py
+++ get_data.py
@@ -1,26 +1,27 @@
+from symbols import symbols
 from alpaca_trade_api.rest import REST, TimeFrame
 import csv
 
-api: REST = REST()
-SYMBOL: str = 'AAPL'
-
+# Select the target symbol:
+TARGET = 'ETHUSD'
+# Select the time period
 START_DATE = '2022-04-01T09:00:00Z'
 END_DATE = '2022-04-30T23:59:59Z'
 
-data = {
-    'training_size': 0,
-    'training': [],
-    'training_ts': [],
-    ##################
-    'testing_size': 0,
-    'testing': [],
-    'testing_ts': [],
-}
 
-bars = api.get_bars_iter(SYMBOL, TimeFrame.Minute, START_DATE, END_DATE, adjustment='raw')
+api: REST = REST()
+SYMBOL: str = TARGET
+SAVE_LOCATION: str = symbols[TARGET]['path']
+
+
+if symbols[TARGET]['is_crypto']:
+    bars = api.get_crypto_bars(TARGET, TimeFrame.Minute, START_DATE, END_DATE)
+else:
+    bars = api.get_bars_iter(SYMBOL, TimeFrame.Minute, START_DATE, END_DATE, adjustment='raw')
+
 
 # CSV
-with open('data/training_data.csv', 'w') as file:
+with open(SAVE_LOCATION, 'w') as file:
     csv_writer = csv.writer(file)
     csv_writer.writerow(['average-price', 'timestamp'])
 
