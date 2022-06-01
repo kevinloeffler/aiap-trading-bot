45100144c328aef767f2505d79070f8533827ad8
Kevin Loeffler <hi@kevinloeffler.ch>
2022-06-01 17:43:41 +0200

    ADD: Act on prediction, crypto stream UPDATE: handle_bar method parameters REMOVE: test price queue, unused code

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from predict import predict
+from symbols import symbols
+import portfolio
 import asyncio
 import websockets
 import json
@@ -6,27 +8,18 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+TARGET_SYMBOL = 'ETHUSD'
+IS_CRYPTO = symbols[TARGET_SYMBOL]['is_crypto']
 
-WS_STREAM_URL = 'wss://stream.data.alpaca.markets/v2/iex'
+WS_STREAM_URL = 'wss://stream.data.alpaca.markets/v1beta1/crypto' if IS_CRYPTO else 'wss://stream.data.alpaca.markets/v2/iex'
 AUTH_MSG = {'action': 'auth', 'key': f'{os.getenv("APCA_API_KEY_ID")}', 'secret': f'{os.getenv("APCA_API_SECRET_KEY")}'}
-SUBSCRIPTION_MSG = {"action": "subscribe", "bars": ["AAPL"]}
-
+SUBSCRIPTION_MSG = {"action": "subscribe", "bars": [TARGET_SYMBOL]}
 
 price_queue = [0] * 60
 
-test_price_queue = [175.29, 175.12, 175.01, 174.94, 174.90, 174.96, 175.05, 175.29, 175.30, 175.31, 175.33, 175.36,
-                    175.33, 175.44, 175.40, 175.49, 175.53, 175.52, 175.50, 175.46, 175.36, 175.31, 175.30, 175.39,
-                    175.44, 175.40, 175.43, 175.50, 175.50, 175.49, 175.50, 175.51, 175.51, 175.50, 175.50, 175.49,
-                    175.48, 175.49, 175.51, 175.52, 175.58, 175.54, 175.59, 175.61, 175.62, 175.50, 175.55, 175.59,
-                    175.56, 175.49, 175.51, 175.56, 175.62, 175.71, 175.64, 175.62, 175.63, 175.68, 175.65, 175.66]
 
-test_bar: dict = {'T': 'b', 'S': 'SPY', 'o': 388.985, 'h': 389.13, 'l': 388.975, 'c': 160.0, 'v': 49378, 't': '2021-02-22T19:15:00Z'}
-
-
-def handle_bar(queue: list, bar: dict = None):
-    if bar is None:
-        bar = test_bar
-    close_price = bar["c"]
+def handle_bar(queue: list, bar: dict):
+    close_price = bar["vw"]
     queue.append(close_price)
     queue.pop(0)
 
@@ -72,15 +65,15 @@ async def start_stream(stream: str):
             if bar['T'] == 'b':
                 handle_bar(queue=price_queue, bar=bar)
                 print(f'Added newest price ({bar["vw"]}) to queue')
-                prediction = predict(price_queue)
-                print('Prediction:', prediction)
+                action, side = predict(price_queue)
+
+                current_holding = portfolio.get_position(TARGET_SYMBOL)
+                new_holding = current_holding * action
+                quantity = round(abs(current_holding - new_holding), symbols[TARGET_SYMBOL]['precision'])
+                print('Quantity:', quantity)
+                portfolio.trade_crypto(symbol=TARGET_SYMBOL, quantity=quantity, side=side)
+
+                print('=== END OF RUN ===')
 
 
 asyncio.run(start_stream(WS_STREAM_URL))
-
-'''
-loop = asyncio.get_event_loop()
-loop.run_until_complete(start_stream(WS_STREAM_URL))
-loop.run_forever()
-loop.run_until_complete(handle_msg(WS_STREAM_URL))
-'''
